abb6318bce602f551af79ffe2fcedb5918086d30
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-27 17:29:24 +0200

    function create_bool_matrix

--- submarines/board.py
+++ submarines/board.py
@@ -7,3 +7,14 @@ def create_matrix(size: int, fill: int = 0):
         mat.append(lst)
     return mat
 
+
+
+def create_bool_matrix(size: int, fill: bool = False):
+    mat = []
+    for i in range(size):
+        lst = []
+        for j in range(size):
+            lst.append(fill)
+        mat.append(lst)
+    return mat
+
