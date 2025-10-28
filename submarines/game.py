d19109ebeb41c5d9a41af72b742801fb649e02c0
Haim Aflalo <kodkodhaim@gmail.com>
2025-10-28 08:42:08 +0200

    function place_random

--- submarines/game.py
+++ submarines/game.py
@@ -8,3 +8,10 @@ def init_game(size,n_ships, max_shots,*, rng) ->dict:
     shot_mat = board.create_bool_matrix(size)
     state = {"size":size,"ships":ship_mat,"shots":shot_mat,"n_ships":n_ships,"max_shots":max_shots,"rng":rng}
     return state
+def shoot(state: dict, x: int, y: int) -> tuple[bool, str]:
+    is_hit = False
+    if board.is_bounds(x,y):
+        if state["ships"][x][y] == 1:
+            return True, "Great you hit a boat !"
+        else:
+            return False,"Nothing here!"
